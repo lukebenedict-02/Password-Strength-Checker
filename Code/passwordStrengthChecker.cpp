2349fd8cf23f421aa782c2d3419e51073e586557
LukeBenedict <u1315472@gcloud.utah.edu>
2026-02-19 21:17:25 +0000

    Completed passwordStrengthChecker method

--- Code/passwordStrengthChecker.cpp
+++ Code/passwordStrengthChecker.cpp
@@ -5,23 +5,39 @@ using std::cin;
 using std::endl;
 using namespace std;
 
+/*
+@brief: This method checks to see if your password is a weak, moderate, or strong password. It gives comments on how you can improve your password if it is weak.
+
+@param: password - A string representing your password
+@return:void
+ */
 void passwordStrengthChecker(string password){
+    // Strength level
+    int score = 0;
     int const passwordLength = password.length();
     // Check length of password
     if(passwordLength == 0) {
         cout << "You didnt put in a password" << endl;
     }
     else if (passwordLength < 5) {
-        cout << "Your password is very short. Try to add more to it." << endl;
+        score++;
+        cout << "Length: very short" << endl;
     }
     else if (passwordLength < 8) {
-        cout << "You have a average password. Try to make it harder to guess." << endl;
+        score += 2;
+        cout << "Length: short" << endl;
     }
     else if (passwordLength < 12) {
-        cout << "Your password has a good length!" << endl;
+        score += 3;
+        cout << "Length: normal" << endl;
     }
     else if (passwordLength >= 12) {
-        cout << "You have a long password!" << endl;
+        score += 4;
+        cout << "Length: good" << endl;
+    }
+    else {
+        score += 5;
+        cout << "Length: great" << endl;
     }
 
     // Check for complexity of the password
@@ -31,18 +47,50 @@ void passwordStrengthChecker(string password){
     for (char c : password) {
         if (isdigit(c)) {
             numberInPassword = true;
+            score++;
         }
         else if (!(isalpha(c))) {
             symbolInPassword = true;
+            score++;
         } 
     }
 
+    // Says whether you have a weak, moderate, or strong password.
+    switch(score){
+        case 0:
+            cout << "You have a very weak password" << endl;
+            break;
+        case 1:
+            cout << "You have a very weak password" << endl;
+            break;
+        case 2:
+            cout << "You have a weak password" << endl;
+            break;
+        case 3:
+            cout << "You have a moderate password" << endl;
+            break;
+        case 4: 
+            cout << "You have a moderate password" << endl;
+            break;
+        case 5:
+            cout << "You have a strong password" << endl;
+            break;
+        case 6:
+            cout << "You have a very strong password" << endl;
+            break;
+        case 7:
+            cout << "You have a very strong password" << endl;
+            break;
+    }
+
+    // Tells whether you should add a number or symbol to your password
     if (symbolInPassword == false) {
-        cout << "You should add one or more symbols to your password." << endl;
+        cout << "You should add one or more symbols to your password" << endl;
     }
     if (numberInPassword == false) {
-        cout << "You should add one or more numbers to your password." << endl;
+        cout << "You should add one or more numbers to your password" << endl;
     }
+
 }
 
 int main() {
